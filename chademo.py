0cba5f8f83b8a3e052fb32abbe5161c7eeb29e68
F1atline <hayevgen93@gmail.com>
2022-04-21 10:53:37 +0300

    add config.json and read settings

--- chademo.py
+++ chademo.py
@@ -6,9 +6,41 @@ from can.notifier import MessageRecipient
 from typing import List
 from datatypes import *
 from enums import *
+import sys
+import json
+
+settings = json.loads(sys.argv[1])
 
 logging.basicConfig(level=logging.DEBUG)
 
+class State:
+    def run(self):
+        assert 0, "run not implemented"
+    def next(self, state):
+        assert 0, "next not implemented"
+
+class StateMachine:
+    def __init__(self, initialState):
+        self.currentState = initialState
+        self.currentState.run()
+    # Template method:
+    def runAll(self, states):
+        for i in states:
+            print(i)
+            self.currentState = self.currentState.next(i)
+            self.currentState.run()
+
+class SourceAction:
+    def __init__(self, action):
+        self.action = action
+    def __str__(self): return self.action
+    def __cmp__(self, other):
+        return cmp(self.action, other.action)
+    # Necessary when __cmp__ or __eq__ is defined
+    # in order to make this class usable as a
+    # dictionary key:
+    def __hash__(self):
+        return hash(self.action)
 class source():
 
     def __init__(self,  support_EV_contactor_welding_detcection: bool = False,
@@ -37,7 +69,7 @@ class source():
         self.remaining_time_of_charging = remaining_time_of_charging
 
         self.canbus = can.Bus(  # type: ignore
-        interface="virtual", channel="vcan0", receive_own_messages=False)
+        interface=str(settings.get('interface_1')), channel=str(settings.get('channel_1')), receive_own_messages=False)
 
         self.reader = can.AsyncBufferedReader()
 
@@ -119,8 +151,8 @@ class consumer:
         self.charged_rate = charged_rate
 
         self.canbus = can.Bus(  # type: ignore
-        interface="virtual", channel="vcan0", receive_own_messages=False)
-
+        interface=str(settings.get('interface_2')), channel=str(settings.get('channel_2')), receive_own_messages=False)
+        
         self.reader = can.AsyncBufferedReader()
 
         self.listeners: List[MessageRecipient] = [
@@ -132,20 +164,45 @@ class consumer:
 
         self.canbus.set_filters(filters)
 
+    def handle_message(self, msg: can.Message) -> None:
+        """Regular callback function. Can also be a coroutine."""
+
+        if msg.arbitration_id == 0x108:
+            if msg.data[0] == 0x00:
+                logging.debug("Identifier of support for EV contactor welding detection: Not supporting EV contactor welding detection")
+            else:
+                logging.debug("Identifier of support for EV contactor welding detection %d", msg.data[0])
+            logging.debug("Available output voltage %d", msg.data[1] | msg.data[2]<<8)
+            logging.debug("Available output current %d", msg.data[3])
+            logging.debug("Threshold voltage %d", msg.data[4] | msg.data[2]<<5)
+
+        if msg.arbitration_id == 0x109:
+            logging.debug("Protocol number %d", msg.data[0])
+            logging.debug("Present output voltage %d", msg.data[1] | msg.data[2]<<8)
+            logging.debug("Present charging current %d", msg.data[3])
+            logging.debug("Status / fault flag %d", msg.data[5])
+            if msg.data[6] == 0xFF:
+                logging.debug("Maximum charging time (by seconds): usage by minute")
+            else:
+                logging.debug("Maximum charging time (by seconds) %d", msg.data[6]*10)
+            logging.debug("Remaining charging time (by by minute) %d", msg.data[7])
+
     def listener(self, msg: can.Message) -> None:
         """Regular callback function. Can also be a coroutine."""
         logging.debug(msg)
 
 async def main() -> None:
     charger = source()
-    charger.listeners.append(charger.listener)
+    charger.listeners.append(charger.listener) 
     charger.listeners.append(charger.handle_message)
     ev = consumer()
+    ev.listeners.append(ev.listener) 
+    ev.listeners.append(ev.handle_message)
     logging.info("Started!")
     # Create Notifier with an explicit loop to use for scheduling of callbacks
     loop=asyncio.get_running_loop()
-    notifier = can.Notifier(charger.canbus, charger.listeners, loop=loop)
-
+    notifier_charger = can.Notifier(charger.canbus, charger.listeners, loop=loop)
+    notifier_ev = can.Notifier(ev.canbus, ev.listeners, loop=loop)
     ev.canbus.send(can.Message( arbitration_id=0x102, 
                                 dlc=8,
                                 data=[  0x0,
@@ -189,8 +246,39 @@ async def main() -> None:
 
     await charger.reader.get_message()
 
+    sleep(1.0)
+    charger.canbus.send(can.Message( arbitration_id=0x108, 
+                                dlc=8,
+                                data=[  0x0,
+                                        0x93,
+                                        0x01,
+                                        0xFF,
+                                        0x93, 
+                                        0x01,
+                                        0x0, 
+                                        0x0 ], 
+                                is_extended_id=False))
+
+    await ev.reader.get_message()
+
+    sleep(1.0)
+    charger.canbus.send(can.Message( arbitration_id=0x109, 
+                                dlc=8,
+                                data=[  0x01,
+                                        0x93,
+                                        0x01,
+                                        0xFF,
+                                        0x0, 
+                                        0xFF,
+                                        0xFF, 
+                                        0x0F ], 
+                                is_extended_id=False))
+
+    await ev.reader.get_message()
+
     # Clean-up
-    notifier.stop()
+    notifier_charger.stop()
+    notifier_ev.stop()
 
 if __name__ == '__main__':
     asyncio.run(main())
