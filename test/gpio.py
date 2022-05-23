b1cffd474c5eee22a1d4cfef63f6ce3a1871ad19
F1atline <hayevgen93@gmail.com>
2022-05-23 14:55:20 +0300

    fix

--- test/gpio.py
+++ test/gpio.py
@@ -28,7 +28,7 @@ class GPIO(pigpio.pi):
         if not self.connected:  # Check connection
             logger.error("Not connected to PIGPIO Daemon")
             sys.exit(1)
-        # else:
+        else:
             self.cb_seq_1 = self.init_pin(pin=sequence_1, pin_direction=pigpio.INPUT, pull_up_resistor=pigpio.PUD_OFF, callback=callback_sequence_1, edge=pigpio.EITHER_EDGE)
             self.cb_seq_2 = self.init_pin(pin=sequence_2, pin_direction=pigpio.INPUT, pull_up_resistor=pigpio.PUD_OFF, callback=callback_sequence_2, edge=pigpio.EITHER_EDGE)
             self.cb_prox = self.init_pin(pin=proximity, pin_direction=pigpio.INPUT, pull_up_resistor=pigpio.PUD_OFF, callback=callback_proximity, edge=pigpio.EITHER_EDGE)
